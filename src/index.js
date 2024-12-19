16db2eed8def74828cfdc047e970a8cc843fe524
Callie Schneider <schcw@Callies-MacBook-Pro-2.local>
2024-12-19 01:26:49 -0800

    Add global black background style

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createGlobalStyle } from 'styled-components';
 import App from './App';
+
+const GlobalStyle = createGlobalStyle`
+  body, html {
+    margin: 0;
+    padding: 0;
+    background-color: #000;
+    min-height: 100vh;
+    overflow-x: hidden;
+  }
+`;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
