3ec1dd2caddd5dd52afef0ed72a4df9fb3d8a310
claws <162635934+callieschneider@users.noreply.github.com>
2024-12-28 02:14:44 -0800

    Update App.js
    reverted

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import styled from 'styled-components';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';
 import Counter from './components/Counter';
 import Home from './pages/Home';
 import Sightings from './pages/Sightings';
@@ -10,36 +9,35 @@ import Research from './pages/Research';
 import Contact from './pages/Contact';
 
 const AppContainer = styled.div`
- background-color: #000;
- color: #0ff;
- min-height: 100vh;
- font-family: 'Courier New', monospace;
- display: flex;
- flex-direction: column;
+  background-color: #000;
+  color: #0ff;
+  min-height: 100vh;
+  font-family: 'Courier New', monospace;
+  display: flex;
+  flex-direction: column;
 `;
 
 const MainContent = styled.main`
- flex: 1;
+  flex: 1;
 `;
 
 function App() {
-return (
-<Router>
-<AppContainer>
-<Navbar />
+  return (
+    <Router>
+      <AppContainer>
+        <Navbar />
         <Counter />
-<MainContent>
-<Routes>
-<Route path="/" element={<Home />} />
-<Route path="/sightings" element={<Sightings />} />
-<Route path="/research" element={<Research />} />
-<Route path="/contact" element={<Contact />} />
-</Routes>
-</MainContent>
-        <Footer />
-</AppContainer>
-</Router>
-);
+        <MainContent>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sightings" element={<Sightings />} />
+            <Route path="/research" element={<Research />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </MainContent>
+      </AppContainer>
+    </Router>
+  );
 }
 
 export default App;
