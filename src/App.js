9ac4850ab953b6b14ddd958a9c72df8bafaaf920
claws <162635934+callieschneider@users.noreply.github.com>
2024-12-28 01:54:53 -0800

    Update App.js
    Updated formatting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import Navbar from './components/Navbar';
 import Counter from './components/Counter';
 import Home from './pages/Home';
@@ -8,23 +8,40 @@ import Sightings from './pages/Sightings';
 import Research from './pages/Research';
 import Contact from './pages/Contact';
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Open Sans', sans-serif;
+    line-height: 1.6;
+  }
+`;
+
+
 const AppContainer = styled.div`
   background-color: #000;
   color: #0ff;
   min-height: 100vh;
-  font-family: 'Courier New', monospace;
   display: flex;
   flex-direction: column;
+`;
+
+const ContentContainer = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
 `;
 
 const MainContent = styled.main`
   flex: 1;
 `;
 
+
 function App() {
   return (
     <Router>
+      <GlobalStyle />
       <AppContainer>
+        <ContentContainer>
         <Navbar />
         <Counter />
         <MainContent>
@@ -35,6 +52,7 @@ function App() {
             <Route path="/contact" element={<Contact />} />
           </Routes>
         </MainContent>
+        </ContentContainer>
       </AppContainer>
     </Router>
   );
