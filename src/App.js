b0f1742a4c2e45e1aa20ee143559888d2ecb110e
claws <162635934+callieschneider@users.noreply.github.com>
2024-12-28 02:12:04 -0800

    Update App.js
    Revert to previous formatting

--- src/App.js
+++ src/App.js
@@ -1,61 +1,45 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import styled, { createGlobalStyle } from 'styled-components';
+import styled from 'styled-components';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Counter from './components/Counter';
 import Home from './pages/Home';
 import Sightings from './pages/Sightings';
 import Research from './pages/Research';
 import Contact from './pages/Contact';
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    font-family: 'Open Sans', sans-serif;
-    line-height: 1.6;
-  }
-`;
-
-
 const AppContainer = styled.div`
-  background-color: #000;
-  color: #0ff;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-`;
-
-const ContentContainer = styled.div`
-  padding: 20px;
-  display: flex;
-  flex-direction: column;
-  flex: 1;
+ background-color: #000;
+ color: #0ff;
+ min-height: 100vh;
+ font-family: 'Courier New', monospace;
+ display: flex;
+ flex-direction: column;
 `;
 
 const MainContent = styled.main`
-  flex: 1;
+ flex: 1;
 `;
 
-
 function App() {
-  return (
-    <Router>
-      <GlobalStyle />
-      <AppContainer>
-        <ContentContainer>
-        <Navbar />
+return (
+<Router>
+<AppContainer>
+<Navbar />
         <Counter />
-        <MainContent>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/sightings" element={<Sightings />} />
-            <Route path="/research" element={<Research />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </MainContent>
-        </ContentContainer>
-      </AppContainer>
-    </Router>
-  );
+<MainContent>
+<Routes>
+<Route path="/" element={<Home />} />
+<Route path="/sightings" element={<Sightings />} />
+<Route path="/research" element={<Research />} />
+<Route path="/contact" element={<Contact />} />
+</Routes>
+</MainContent>
+        <Footer />
+</AppContainer>
+</Router>
+);
 }
 
 export default App;
